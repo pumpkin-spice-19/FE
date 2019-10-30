a261cb98d7a9fe02e5af04cd6f788a80dc489f63
john2796 <jbmiranda22796@gmail.com>
2019-10-30 03:24:42 -0700

    added MU

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,12 +1,29 @@
 import React from "react"
+import {
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendarCheck,
+  FaCircle
+} from "react-icons/fa"
+import { Addproject } from "../Addproject"
 
 export const Sidebar = () => {
   return (
     <div>
-      <p>Inbox </p>
-      <p>Today </p>
-      <p>Next 7 days </p>
+      <p>
+        <FaInbox />
+        Inbox
+      </p>
+      <p>
+        <FaRegCalendarCheck />
+        Today
+      </p>
+      <p>
+        <FaRegCalendarAlt />
+        Next 7 days
+      </p>
       <p>+ Add Project</p>
+      <Addproject />
     </div>
   )
 }
